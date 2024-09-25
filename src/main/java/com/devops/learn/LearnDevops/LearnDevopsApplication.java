6928818f0b3ded38da69c950d3d923ceffe5b802
HarshitSinghai <harshitksinghai.official@gmail.com>
2024-09-25 08:40:41 +0530

    hello world

---
+++ src/main/java/com/devops/learn/LearnDevops/LearnDevopsApplication.java
@@ -0,0 +1,14 @@
+package com.devops.learn.LearnDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDevopsApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/devops/learn/LearnDevops/LearnDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.learn.LearnDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
